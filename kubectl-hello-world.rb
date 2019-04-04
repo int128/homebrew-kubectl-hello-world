3af5e3f79849115cc8fa670a149c2750dd614e43
Hidetake Iwata <int128@gmail.com>
2019-04-04 15:35:24 +0900

    v1.0.4

--- kubectl-hello-world.rb
+++ kubectl-hello-world.rb
@@ -3,7 +3,7 @@ class KubectlHelloWorld < Formula
   homepage "https://github.com/int128/kubectl-hello-world"
   url "https://github.com/int128/kubectl-hello-world/releases/download/v1.0.4/kubectl-hello_world_darwin_amd64.zip"
   version "v1.0.4"
-  sha256 "ce32a11e1841791fff66af3870af72bde0c1b7c3bb16f8259a003d8c5428b23e"
+  sha256 "f37075594f096ba95f6a11e32eddcb93d0cc9c02d9e3a3793b3e547f91becea8"
 
   def install
     bin.install "kubectl-hello_world"
